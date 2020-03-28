c512fbca927de427fd2a59cb977f83a89cd5c435
shaongithub <cse.aishaon@gmail.com>
2020-03-28 07:20:31 +0600

    Register No Change to ID NO

---
+++ mvc/logs/log-2020-03-28.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-03-28 00:35:25 --> Could not find the language line "idcardreport_idNO"
+ERROR - 2020-03-28 00:35:25 --> Could not find the language line "idcardreport_idNO"
+ERROR - 2020-03-28 00:35:28 --> Could not find the language line "idcardreport_idNO"
+ERROR - 2020-03-28 00:35:28 --> Could not find the language line "idcardreport_idNO"
+ERROR - 2020-03-28 00:35:37 --> Could not find the language line "idcardreport_idNO"
+ERROR - 2020-03-28 00:35:37 --> Could not find the language line "idcardreport_idNO"
